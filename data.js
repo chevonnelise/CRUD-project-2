65912f4f2d27fefe5a51a12ad3b60ad9f52110ff
chevonnelise <elise.chevonne@gmail.com>
2024-02-12 16:51:42 +0000

    initial commit

---
+++ data.js
@@ -0,0 +1,66 @@
+// add function
+const BASE_JSON_BIN_URL = "https://api.jsonbin.io/v3/b";
+const BIN_ID = "65c59f4f266cfc3fde87d272";
+const MASTER_KEY = "$2a$10$bGGtgHiNbK3z4vgOgJ28J.fxGlg8OD9LqM/yfLGJp7zF6XnFV0hoa";
+
+let todos = [];
+
+function addTodo(todos, name, urgency){
+    let newTodo = {
+        id: Math.floor(Math.random() * 100 + 1),
+        name: name,
+        urgency: urgency
+    };
+    todos.push(newTodo);
+}
+
+function modifyTask(todos, id, newName, newUrgency) {
+    // creating the new task
+    let modifiedTask = {
+        "id": id,
+        "name": newName,
+        "urgency": newUrgency
+    }
+
+    // insert index of the task that user wants to replace
+    const indexToReplace = todos.findIndex(function(t){
+        return t.id == id;
+    });
+
+    // if loop to check if the findIndex can find the index, if not findIndex to return -1
+    if (indexToReplace > -1) {
+        todos[indexToReplace] = modifiedTask;
+    }
+}
+
+function deleteTask(todos, id) {
+    let indexToDelete = null;
+    for (let i = 0; i < todos.length; i++){
+        if (todos[i].id == id){
+            indexToDelete = i;
+            break;
+        }
+    }
+    if (indexToDelete !== null) {
+        todos.splice(indexToDelete, 1);
+    } else {
+        console.log("Unable to find task");
+    }
+}
+
+async function loadTasks() {
+    const response = await axios.get(BASE_JSON_BIN_URL + "/" + BIN_ID + "/latest");
+    console.log(response.data);
+    return response.data.record;
+}
+
+
+async function saveTasks(todos) {
+    const response = await axios.put(`${BASE_JSON_BIN_URL}/${BIN_ID}`, todos, {
+        headers: {
+            "Content-Type": "application/json",
+            "X-Master-Key": MASTER_KEY
+        }
+    });
+    return response.data;
+}
---
+++ script.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+    async function main() {
+        let todos = await loadTasks();
+
+        const addTodoButton = document.querySelector("#addTodo");
+        addTodoButton.addEventListener('click', function (){
+        // add Event listeners
+        // const form = document.querySelector("#todo-form");
+        // form.addEventListener('submit', function (event) {
+        //     event.preventDefault(); // This stops the form from being submitted the traditional way
+        
+        const saveButton = document.querySelector("#save-btn");
+            saveButton.addEventListener("click", async function (){
+                saveTasks(todos);
+            })
+            
+            const taskNameInput = document.querySelector("#taskName");
+            const taskName = taskNameInput.value;
+
+            const taskUrgencySelect = document.querySelector("#taskUrgency");
+            const taskUrgency = taskUrgencySelect.value;
+
+            if (taskName) {
+                addTodo(todos, taskName, taskUrgency);
+                renderTodos(todos);
+                taskNameInput.value = '';
+        }
+    });
+    //     )
+    };
+
+    function renderTodos(todos) {
+        const todoList = document.querySelector('#todoList');
+        todoList.innerHTML = '';
+        for (let todo of todos) {
+            const li = document.createElement('li');
+            li.className = 'list-group-item d-flex justify-content-between align-items-center';
+            li.innerHTML = `
+            ${todo.name} <span class="badge  bg-primary">${todo.urgency}</span>
+            <button data-task-id=${todo.id} class="btn edit-btn btn-success btn-sm">Edit</button>
+            <button data-task-id=${todo.id} class="btn delete-btn btn-danger btn-sm">Delete</button>
+            `;
+            todoList.appendChild(li);
+
+            li.querySelector(".edit-btn").addEventListener('click', function () {
+                const newName = prompt("Enter the new task name: ", todo.name);
+                const newUrgency = prompt("Enter the new urgency: ", todo.urgency);
+                modifyTask(todos, todo.id, newName, newUrgency);
+                renderTodos(todos);
+            })
+
+            li.querySelector(".delete-btn").addEventListener('click', function () {
+                const confirmation = confirm("Do you want to delete the task: " + todo.name + "?");
+                if (confirmation) {
+                    deleteTask(todos, todo.id);
+                    renderTodos(todos);
+                }
+            });
+        }
+    };
+
+    // function main() {
+    //     let todos = [];
+
+    //     const addTodoButton = document.querySelector("#addTodo");
+    //     addTodoButton.addEventListener('click', function () {
+    //         const taskNameInput = document.querySelector("#taskName")
+    //         const taskName = taskNameInput.value;
+
+    //         const taskUrgencySelect = document.querySelector("#taskUrgency");
+    //         const taskUrgency = taskUrgencySelect.value;
+
+    //         if (taskName) {
+    //             addTodo(todos, taskName, taskUrgency);
+    //             renderTodos(todos);
+    //             taskNameInput.value = '';
+    //         }
+    //     });
+    // }
+    main();
+});
+
+
